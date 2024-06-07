5b0080a743714ee730cc80f37fcfc2174ac3c726
chloe de campos <chloe_de_campos@brown.edu>
2024-06-06 22:53:13 -0400

    op

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Other ESLint configurations...
+    rules: {
+      // Other rules...
+      'no-unused-vars': 'warn', // Treat unused variables as warnings
+      'react-hooks/exhaustive-deps': 'warn', // Treat missing useEffect dependencies as warnings
+      // Other rules...
+    },
+  };
